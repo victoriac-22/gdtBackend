6a171b2e0ea398de641c2a1c172bee5495958960
victoriac-22 <cheng_victoria@yahoo.com>
2022-10-30 19:14:50 -0400

    code with nodemon issue

--- App.js
+++ App.js
@@ -1,5 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// StatusBar = require('expo-status-bar');
+// StyleSheet = require('react-native');
+// Text = require('react-native');
+// View = require ('react-native');
+
+// const express = require('express');
+// const mysql = require('mysql');
+
+// const app = express();
+// app.listen('3000',() => {
+//   console.log('Server started on port 3000');
+// });
+
+
 
 export default function App() {
   return (
